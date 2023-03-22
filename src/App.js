74d7609375840f61e28f00d878b9c79dc94b0a53
Svmpankaj <svmpankaj@gmail.com>
2023-03-22 23:23:15 +0530

    fixed npm package error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import { Button } from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 function App() {
   return (
     <div >
       <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <Button
+        startIcon={<SettingsIcon />}
+        variant="contained"
+        color='secondary'
+        size='small'
+      >
+        Contained
+      </Button>
+      <Button
+        startIcon={<SettingsIcon />}
+        variant="contained"
+        color='success'
+        size='small'
+      >
+        Contained
+      </Button>
+
+      <Button variant="outlined" disabled>Outlined</Button>
     </div>
   );
 }
 
 export default App;
+
