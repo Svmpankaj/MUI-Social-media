da024183722084b963d335d74c0bf0c6b79bdb07
Svmpankaj <svmpankaj@gmail.com>
2023-03-24 01:53:42 +0530

    added sidebar list

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
+import React, { useState } from 'react'
 import { Mail, Notifications, Pets } from '@mui/icons-material'
-import { AppBar, Avatar, Badge, Box, InputBase, styled, Toolbar, Typography } from '@mui/material'
-import React from 'react'
+import { AppBar, Avatar, Badge, Box, InputBase, Menu, MenuItem, styled, Toolbar, Typography } from '@mui/material'
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
@@ -15,18 +15,27 @@ const Search = styled("div")(({theme}) => ({
 }));
 
 const Icons = styled(Box)(({theme}) => ({
-  display: 'flex', 
+  display: 'none', 
   alignItems: 'center', 
-  gap: "20px"
+  gap: "20px",
+  [theme.breakpoints.up("sm")]:{
+    display: "flex"
+  }
 }));
 
 const UserBox = styled(Box)(({theme}) => ({
   display: 'flex', 
   alignItems: 'center', 
-  gap: "10px"
+  gap: "10px",
+  [theme.breakpoints.up("sm")]:{
+    display: "none"
+  }
 }));
 
 const Navbar = () => {
+
+  const [open, setOpen] = useState(false)
+
   return (
     <AppBar position='sticky'>
       <StyledToolbar>
@@ -42,15 +51,36 @@ const Navbar = () => {
         </Badge>
         <Avatar 
         sx={{width: 30, height: 30}} 
-        src='https://static.photocdn.pt/images/articles/2019/08/07/images/articles/2019/07/31/linkedin_profile_picture_tips-1.webp'/>
+        src='https://static.photocdn.pt/images/articles/2019/08/07/images/articles/2019/07/31/linkedin_profile_picture_tips-1.webp'
+        onClick={(e) =>setOpen(true)}
+        />
       </Icons>
-      <UserBox>
+      <UserBox onClick={(e) =>setOpen(true)}>
       <Avatar 
         sx={{width: 30, height: 30}} 
         src='https://static.photocdn.pt/images/articles/2019/08/07/images/articles/2019/07/31/linkedin_profile_picture_tips-1.webp'/>
         <Typography variant='span'>John</Typography>
       </UserBox>
       </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+      >
+        <MenuItem >Profile</MenuItem>
+        <MenuItem >My account</MenuItem>
+        <MenuItem >Logout</MenuItem>
+      </Menu>
+
     </AppBar>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,73 @@
-import { Box } from "@mui/material";
+import { AccountBox, Article, Group, Home, Person, Settings, Storefront } from "@mui/icons-material";
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import React from "react";
 
 const Sidebar = () => {
   return (
     <Box
-      bgcolor="skyblue"
+
       flex={1}
       p={2}
       sx={{ display: { xs: "none", sm: "block" } }}
     >
-      Sidebar
+      <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#Home">
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Homepage" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <Article />
+              </ListItemIcon>
+              <ListItemText primary="Pages" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <Group />
+              </ListItemIcon>
+              <ListItemText primary="Groups" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <Storefront />
+              </ListItemIcon>
+              <ListItemText primary="Marketplace" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <Person />
+              </ListItemIcon>
+              <ListItemText primary="Friends" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <Settings />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <AccountBox />
+              </ListItemIcon>
+              <ListItemText primary="Profile" />
+            </ListItemButton>
+          </ListItem>
+      </List>
     </Box>
   );
 };
